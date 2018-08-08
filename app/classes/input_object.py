0c2b6e1f6087423e1ea8b18c0796f9256abe9cee
Edward Brennan <EdMan1022@gmail.com>
2018-08-08 11:56:36 -0400

    Add `InputController` that maps an `InputObject` to a physics object
    Also executes the read_input method, that adds the input_object's force to the physics_controller's force queue if the button was pressed

--- app/classes/input_object.py
+++ app/classes/input_object.py
@@ -15,3 +15,32 @@ class InputObject(object):
         self.force = force
 
 
+class InputController(object):
+    """
+    Binds a defined button input to a single physics object
+    """
+
+    def __init__(self, input_objects: list, physics_object: PhysicsObject):
+        self.input_objects = input_objects
+        self.physics_object = physics_object
+
+        # Disables the controller's forces for all objects except the bound one
+        for input_object in self.input_objects:
+            input_object.force.item_array = np.zeros(
+                physics_object.physics_controller.n_objects)
+            input_object.force.item_array[self.physics_object.index] = 1
+
+    def read_input(self):
+        for input_object in self.input_objects:
+            try:
+                for button in input_object.button:
+                    if pyxel.btn(button):
+                        self.physics_object.physics_controller. \
+                            temporary_forces.append(
+                            input_object.force)
+                        break
+            except TypeError:
+                if pyxel.btn(input_object.button):
+                    self.physics_object.physics_controller.temporary_forces.append(
+                        input_object.force
+                    )
